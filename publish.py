c062c2b54781c1f1559e2c15aeaf59e109f52fdd
Vic D <dvv112345@gmail.com>
2024-11-23 14:23:13 -0500

    Make publish.py easier to use

--- publish.py
+++ publish.py
@@ -5,6 +5,9 @@ from std_msgs.msg import Float64MultiArray, Float64, Int32MultiArray, Bool
 from util import cvCallback, depthCallback, gyroCallback, cv, turn, changeDepth, searchGate, move, moveTillGone, distanceCallback
 
 
+DEPTH = 2
+
+
 if __name__ == '__main__':
     rospy.init_node('publish')
 
@@ -16,32 +19,31 @@ if __name__ == '__main__':
 
     #prevyear
     cvBottomPub = rospy.Publisher('CV_bottom', Float64MultiArray)
-    
-    
-    initial_CV = Float64MultiArray()
-    initial_CV.data = []
-    initial_depth = Float64(2.0)
-    initial_gyro = Float64MultiArray()
-    initial_gyro.data = [0, 0, 0]
-    initial_distance = Float64MultiArray()
-    initial_distance.data = [0, 0, 0]
-    initial_CV_bottom = Float64MultiArray()
-    initial_CV_bottom.data = []
-    initial_pressure = Float64(0)
-    rospy.sleep(1)
-    print("Sending initial values")
-    cvPub.publish(initial_CV)
-    depthPub.publish(initial_depth)
-    gyroPub.publish(initial_gyro)
-    distancePub.publish(initial_distance)
-    cvBottomPub.publish(initial_CV_bottom)
-    pressurePub.publish(initial_pressure)
      
 
 
     while not rospy.is_shutdown():
         topic = input("Topic: ").upper()
         msg = input("Message: ")
+        if topic == "setup":
+            initial_CV = Float64MultiArray()
+            initial_CV.data = []
+            initial_depth = Float64(DEPTH)
+            initial_gyro = Float64MultiArray()
+            initial_gyro.data = [0, 0, 0]
+            initial_distance = Float64MultiArray()
+            initial_distance.data = [0, 0, 0]
+            initial_CV_bottom = Float64MultiArray()
+            initial_CV_bottom.data = []
+            initial_pressure = Float64(0)
+            rospy.sleep(1)
+            print("Sending initial values")
+            cvPub.publish(initial_CV)
+            depthPub.publish(initial_depth)
+            gyroPub.publish(initial_gyro)
+            distancePub.publish(initial_distance)
+            cvBottomPub.publish(initial_CV_bottom)
+            pressurePub.publish(initial_pressure)
         if topic == "CV":
             msg = ast.literal_eval(msg)
             cvMsg = Float64MultiArray()
@@ -49,8 +51,11 @@ if __name__ == '__main__':
             cvPub.publish(cvMsg)
         elif topic == "DEPTH":
             depthF = float(msg)
+            pressureF = DEPTH - depthF
+            pressureMsg = Float64(pressureF)
             depthMsg = Float64(depthF)
             depthPub.publish(depthMsg)
+            pressurePub.publish(pressureMsg)
         elif topic == "GYRO":
             msg = ast.literal_eval(msg)
             gyroMsg = Float64MultiArray()
@@ -66,10 +71,6 @@ if __name__ == '__main__':
             cvBottomMsg = Float64MultiArray()
             cvBottomMsg.data = msg
             cvBottomPub.publish(cvBottomMsg)
-        elif topic == "PRESSURE":
-            pressureF = float(msg)
-            pressureMsg = Float64(pressureF)
-            pressurePub.publish(pressureMsg)
         else:
             print("Invalid topic")
 
