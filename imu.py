c8d43233b835c110a86e704d54c2c1c0175771b6
Vic D <dvv112345@gmail.com>
2025-05-03 17:29:17 -0400

    new imu import

--- imu.py
+++ imu.py
@@ -10,6 +10,8 @@ Download module from https://github.com/LORD-MicroStrain/MSCL/blob/master/HowToU
 """
 
 # from win64_mscl import mscl
+import sys
+sys.path.append('./linux_mscl/')
 from linux_mscl import mscl
 import rospy
 from std_msgs.msg import Float64MultiArray
