d271f7715ee995566acc5433eda16bc01768c2cb
Vic D <dvv112345@gmail.com>
2025-08-12 03:55:17 +1000

    remove thruster from front ard

--- front_arduino/front_arduino.ino
+++ front_arduino/front_arduino.ino
@@ -44,62 +44,62 @@ float z_disp = 0;
 int brightness = 1600;
 
 
-void turnLeft(const int value)
-{
-  // trusterFR.writeMicroseconds(backward);
-  trusterBR.writeMicroseconds(noMove + value);
-  // trusterFL.writeMicroseconds(noMove + value);
-  // trusterBL.writeMicroseconds(backward);  
-}
+// void turnLeft(const int value)
+// {
+//   // trusterFR.writeMicroseconds(backward);
+//   trusterBR.writeMicroseconds(noMove + value);
+//   // trusterFL.writeMicroseconds(noMove + value);
+//   // trusterBL.writeMicroseconds(backward);  
+// }
 
-void turnRight(const int value)
-{
-  // trusterFR.writeMicroseconds(noMove + value);
-  // trusterBR.writeMicroseconds(backward);
-  // trusterFL.writeMicroseconds(backward);
-  trusterBL.writeMicroseconds(noMove + value);  
-}
+// void turnRight(const int value)
+// {
+//   // trusterFR.writeMicroseconds(noMove + value);
+//   // trusterBR.writeMicroseconds(backward);
+//   // trusterFL.writeMicroseconds(backward);
+//   trusterBL.writeMicroseconds(noMove + value);  
+// }
 
-void goBackward(const int value)
-{
-  trusterBR.writeMicroseconds(noMove + value);
-  trusterBL.writeMicroseconds(noMove + value);  
-}
+// void goBackward(const int value)
+// {
+//   trusterBR.writeMicroseconds(noMove + value);
+//   trusterBL.writeMicroseconds(noMove + value);  
+// }
 
-void motorCallback(const std_msgs::Int32MultiArray& msg)
-{
-  if (msg.data[1] > forward_max)
-  {
-    msg.data[1] = forward_max;
-  }
-  if (msg.data[1] < backward_max)
-  {
-    msg.data[1] = backward_max;
-  }
-  if (msg.data[0] == 1)
-  {
-    if (msg.data[1] > 0)
-    {
-      turnRight(msg.data[1]);
-    }
-    else if (msg.data[1] < 0)
-    {
-      turnLeft(abs(msg.data[1]));
-    }
-    else
-    {
-      turnLeft(0);
-      turnRight(0);
-    }
-  }
-  else if (msg.data[0] == 0)
-  {
-    if (msg.data[1] <= 0)
-    {
-      goBackward(abs(msg.data[1]));
-    }
-  }
-}
+// void motorCallback(const std_msgs::Int32MultiArray& msg)
+// {
+//   if (msg.data[1] > forward_max)
+//   {
+//     msg.data[1] = forward_max;
+//   }
+//   if (msg.data[1] < backward_max)
+//   {
+//     msg.data[1] = backward_max;
+//   }
+//   if (msg.data[0] == 1)
+//   {
+//     if (msg.data[1] > 0)
+//     {
+//       turnRight(msg.data[1]);
+//     }
+//     else if (msg.data[1] < 0)
+//     {
+//       turnLeft(abs(msg.data[1]));
+//     }
+//     else
+//     {
+//       turnLeft(0);
+//       turnRight(0);
+//     }
+//   }
+//   else if (msg.data[0] == 0)
+//   {
+//     if (msg.data[1] <= 0)
+//     {
+//       goBackward(abs(msg.data[1]));
+//     }
+//   }
+// }
 
 ros::NodeHandle nh;
 std_msgs::Bool leak_val;
